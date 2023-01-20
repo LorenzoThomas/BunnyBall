b084bb486ad96587eedefec066d7ed0debc00129
LorenzoThomas <97693042+LorenzoThomas@users.noreply.github.com>
2023-01-20 00:14:45 -0500

    Add files via upload
    Module 4

---
+++ GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameOver;//false, true 
+    public GameObject winnerUI;
+    public GameObject player;
+
+    
+    //PascalCase
+    private void Update()
+
+    {
+        if (gameOver == true) {
+            GameOver();
+        }
+    }
+
+    void GameOver() {
+        player.GetComponent<Player>().enabled = false;
+        Destroy(player.GetComponent<Rigidbody>());
+        winnerUI.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Transform cameraTransform;
+    public GameManager gameManager;
+    //create a speed variable
+    public byte speed;
+    
+    void Update()
+    {
+        PlayerMovement();
+      
+   
+    }
+
+    void PlayerMovement() {
+        //Reads input from the user 
+    
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Moves the character on scene
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+        Vector3 direction = forward * moveVertical + right * moveHorizontal;
+        rb.AddForce(direction * speed);// replace number 5 with a variable
+    }
+}
